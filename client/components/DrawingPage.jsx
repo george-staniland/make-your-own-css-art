e7b190dd96ed0c5a1c1fb9f48aa65f7a33e46b52
George-Staniland <georgestaniland@gmail.com>
2020-08-19 11:56:06 +1200

    fix background non-clickable issue

--- client/components/DrawingPage.jsx
+++ client/components/DrawingPage.jsx
@@ -15,10 +15,6 @@ class DrawingPage extends React.Component {
         paintBucketCurrent: ''
     }
 
-
-    // clicking the h1 text box add its Colour to local state
-    // a little circle in top right displays that colour
-
     getColour = (id) => {
         const currentCol = document.getElementById(id).style.color 
         this.setState({
@@ -36,6 +32,9 @@ class DrawingPage extends React.Component {
                 <div id="colourOptions">
                     <h1 id="bisque" onClick={ () => this.getColour("bisque")} style={{ color: 'bisque' }} >Bisque</h1>
                     <h1 id="blue" onClick={ () => this.getColour("blue")} style={{ color: 'cornflowerblue' }}>Cornflower Blue</h1>
+                    <h1 id="LightSalmon" onClick={ () => this.getColour("LightSalmon")} style={{ color: 'LightSalmon' }}>Light Salmon</h1>
+                    <h1 id="Tomato" onClick={ () => this.getColour("Tomato")} style={{ color: 'Tomato' }}>Tomato</h1>
+                    <h1 id="PeachPuff" onClick={ () => this.getColour("PeachPuff")} style={{ color: 'PeachPuff' }}>PeachPuff</h1>
                     <div id="paintBucketCircle" style={{ background: this.state.paintBucketCurrent }}> <p>Selected Colour:</p></div>
 
                 </div>
