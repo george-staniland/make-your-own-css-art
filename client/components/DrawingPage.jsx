72a1ab736cd249efa9bbf077f3ece55465f3f457
George-Staniland <georgestaniland@gmail.com>
2020-08-19 12:09:16 +1200

    implent font inline styles

--- client/components/DrawingPage.jsx
+++ client/components/DrawingPage.jsx
@@ -30,14 +30,20 @@ class DrawingPage extends React.Component {
                 <button id="homeButton"><Link to="/">Home</Link></button>
 
                 <div id="colourOptions">
-                    <h1 id="bisque" onClick={ () => this.getColour("bisque")} style={{ color: 'bisque' }} >Bisque</h1>
+                    <h1 id="bisque" onClick={ () => this.getColour("bisque")} style={{fontFamily: 'BalooTamma2', color: 'bisque' }} >Bisque</h1>
+
                     <h1 id="blue" onClick={ () => this.getColour("blue")} style={{ color: 'cornflowerblue' }}>Cornflower Blue</h1>
+
                     <h1 id="LightSalmon" onClick={ () => this.getColour("LightSalmon")} style={{ color: 'LightSalmon' }}>Light Salmon</h1>
+
                     <h1 id="Tomato" onClick={ () => this.getColour("Tomato")} style={{ color: 'Tomato' }}>Tomato</h1>
+
                     <h1 id="PeachPuff" onClick={ () => this.getColour("PeachPuff")} style={{ color: 'PeachPuff' }}>PeachPuff</h1>
+
                     <div id="paintBucketCircle" style={{ background: this.state.paintBucketCurrent }}> <p>Selected Colour:</p></div>
 
                 </div>
+
                 <div id="drawingSection">
                     <BasicSVG paintBucketColour={this.state.paintBucketCurrent} />
                 </div>
